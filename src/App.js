d7efe9994e0bfeb52c5fda75b8cce466dfe1e603
BhagyasriDasari <106796145+BhagyasriDasari@users.noreply.github.com>
2025-01-23 14:04:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TableComponent from './components/TableComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='main-heading'>Dynamic Table</h1>
+      <TableComponent />
+    </div>
+  );
+}
+
+export default App;
